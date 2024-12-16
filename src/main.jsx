0d32692507015cf66692ea56a75ee19e6682c67b
Punith1997 <punith1097@gmail.com>
2024-12-16 12:28:44 +0530

    Revert "changing codes"
    This reverts commit a20409b723ee1680f74fa1a15a75b26eef9b4a2a.

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './index.css'
 
-// if ('serviceWorker' in navigator) {
-//   console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/serviceWorker.js')
-//       .then((registration) => {
-//         console.log('Service Worker registered:', registration);
-//       })
-//       .catch((error) => {
-//         console.error('Service Worker registration failed:', error);
-//       });
-//   });
-// }
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
